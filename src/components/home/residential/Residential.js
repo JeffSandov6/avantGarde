12b5afa1af0401e655e2bbd48860770a98482616
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2021-08-27 12:01:25 -0500

    residential & commercial layout finished

--- src/components/home/residential/Residential.js
+++ src/components/home/residential/Residential.js
@@ -3,6 +3,7 @@ import { Row, Col } from 'reactstrap';
 
 import residentialImage from './Commercial_8.jpeg';
 import '../CommonHomeComponents.css';
+import './Residential.css';
 
 class Residential extends Component {
 
@@ -10,8 +11,38 @@ class Residential extends Component {
         return (
             <>
             <div style={{ height: '10vh' }}></div>
+            <div>
+                <img 
+                    // className="img-fluid float-left mr-4" 
+                    className="image-float-residential mr-4"
+                    src={residentialImage} 
+                    width="40%"
+                />
+                <div className="residential-commercial-headers">Residential</div>
+                <div className="residential-commercial-supporting-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                    Lorem ipsum dolor sit amet, consectLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                    Pellentesque habitant morbi tri
+                </div>
+            </div>
             
-            <Col className="col-md-6 m-3">
+            {/* <Col className="col-md-6 m-3">
                 <img 
                     className="img-fluid float-left mr-4" 
                     src={residentialImage} 
@@ -39,7 +70,7 @@ class Residential extends Component {
                 Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
                 Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
                 Pellentesque habitant morbi tri
-            </Col>
+            </Col> */}
 
             
             </>
