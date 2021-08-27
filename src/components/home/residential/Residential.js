3453e886ef8f2af8f9152e2dee4816dee10c648c
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2021-08-27 12:01:58 -0500

    deleting some commented out code

--- src/components/home/residential/Residential.js
+++ src/components/home/residential/Residential.js
@@ -41,38 +41,6 @@ class Residential extends Component {
                     Pellentesque habitant morbi tri
                 </div>
             </div>
-            
-            {/* <Col className="col-md-6 m-3">
-                <img 
-                    className="img-fluid float-left mr-4" 
-                    src={residentialImage} 
-                    width="100%"
-                />
-            </Col>
-            <Col className="col-md-12">
-                <div className="residential-commercial-headers">Residential</div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                Lorem ipsum dolor sit amet, consectLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                Pellentesque habitant morbi tri
-            </Col> */}
-
-            
             </>
         )
     }
