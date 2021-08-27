b122e9fec80b2b7b9b4ef46abf245a4f2abafa97
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2021-08-27 11:46:51 -0500

    THIS COMMIT SHOWS THE 2 DIFFERENT WAYS OF WRAPPING TEXT AROUND IMAGES

---
+++ src/components/home/commercial/Commercial.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+
+import commercialImage from './Commercial_8.jpeg';
+import '../CommonHomeComponents.css';
+import './Commercial.css';
+
+class Commercial extends Component {
+
+    render() {
+        return (
+            <>
+            <div style={{ height: '10vh' }}></div>
+            <div>
+                <img 
+                    // className="img-fluid float-left mr-4" 
+                    className="image-float ml-4"
+                    src={commercialImage} 
+                    width="50%"
+                />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                Lorem ipsum dolor sit amet, consectLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                Pellentesque habitant morbi tri
+            </div>
+            </>
+        )
+    }
+}
+
+export default Commercial;
