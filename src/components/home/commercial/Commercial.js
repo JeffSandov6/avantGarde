00e1b1fca973e54cd03c4075241bc16b4d98cc34
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2021-09-17 13:56:34 -0500

    REMOVING THE TEXT WRAP AROUND ON PIC

--- src/components/home/commercial/Commercial.js
+++ src/components/home/commercial/Commercial.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
 
 import commercialImage from './Commercial_8.jpeg';
 import '../CommonHomeComponents.css';
@@ -9,49 +11,30 @@ class Commercial extends Component {
     render() {
         return (
             <>
+            <Row>
             <div style={{ height: '10vh' }}></div>
             <div>
-                <img 
-                    className="image-float-commercial ml-4"
-                    src={commercialImage} 
-                    width="40%"
-                />
-                <div className="residential-commercial-headers">Commercial</div>
-                <div className="residential-commercial-supporting-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Pellentesque habitant morbi tri
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
-                    Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
-                    Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
-                    Lorem ipsum dolor sit amet, consectetur 
-                </div>
+                <Col>
+                    <img 
+                        className="image-float-commercial ml-4"
+                        src={commercialImage} 
+                        width="40%"
+                    />
+                </Col>
+                <Col>
+                    <div className="residential-commercial-headers">Commercial</div>
+                    <div className="residential-commercial-supporting-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                        Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                        Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                        Integer eget porta metus, ac ultricies eros. Etiam aliquet sodales gravida. Integer neque mauris, malesuada sit amet ante ac, sagittis elementum lacus. 
+                        Nulla tellus nibh, tempus et elit vel, lobortis hendrerit felis.
+                        Lorem ipsum dolor sit amet, consectLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit risus ut massa ornare pellentesque. Proin id leo id arcu placerat tincidunt.
+                    </div>
+                </Col>
             </div>
+            </Row>
             </>
         )
     }
