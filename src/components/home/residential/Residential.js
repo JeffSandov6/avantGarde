a2a3568592b8cef2b0d02b7757eee80e76fc6e42
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2021-09-24 16:57:44 -0500

    Fade combined with on scroll

--- src/components/home/residential/Residential.js
+++ src/components/home/residential/Residential.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 import residentialImage from './Commercial_8.jpeg';
 import '../CommonHomeComponents.css';
 import './Residential.css';
 import { Row, Col } from 'react-bootstrap';
+import Fade from '@material-ui/core/Fade';
+import Slide from '@material-ui/core/Slide';
+
 
 
 class Residential extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            elementIsVisible: true,
+        }
+    }
+
+    isVisible = (isVisible) => {
+        console.log("at is visible");
+        console.log(isVisible);
+        this.setState({elementIsVisible: isVisible});
+    }
 
     render() {
         return (
@@ -15,11 +31,23 @@ class Residential extends Component {
                 <div style={{ height: '10vh' }}></div>
                 <div>
                     <Col>
-                        <img 
-                            className="image-float-residential mr-4"
-                            src={residentialImage} 
-                            width="40%"
-                        />
+                        <VisibilitySensor
+                            onChange={this.isVisible}
+                        >
+                            <Fade
+                                in={this.state.elementIsVisible}
+                                timeout={3000}
+                                disableStrictModeCompat={true}
+                                // direction="right"
+                            >
+                                <img 
+                                    className="image-float-residential mr-4"
+                                    src={residentialImage} 
+                                    width="40%"
+                                />
+                            </Fade>
+                        </VisibilitySensor>
+
                     </Col>
                     <Col className="justify-content-center">
                         <div className="residential-commercial-headers">Residential</div>
