b06d0f152645033a76cdb485ba27ec31646cf6c8
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2021-10-15 13:14:17 -0500

    combined request reason and job type

--- src/components/requestWrapForm/requestWrapFormSteps/RequestReasonStep.js
+++ src/components/requestWrapForm/requestWrapFormSteps/RequestReasonStep.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
-import { FormControl, Form } from 'react-bootstrap';
+import { FormControl, Form, Row } from 'react-bootstrap';
 
 
 class RequestReasonStep extends Component {
 
+
+    onChange = (e) => {
+        console.log("hi");
+        console.log(e);
+        console.log(e.target.value);
+    }
+
     render() {
         return (
             <>
+                <Form.Label>Job Type</Form.Label>
+                <Row>
+                    <Form>
+                        <Form.Check
+                            className="radio-button-text"
+                            inline
+                            type='radio'
+                            label='Commercial'
+                            value="Commercial"
+                            checked={this.props.commercialOrResidential === 'Commercial'}
+                            onChange={e => this.props.setCommercialOrResidential(e.target.value)}
+                        />
+                        <Form.Check
+                            className="radio-button-text"
+                            inline
+                            type='radio'
+                            label='Residential'
+                            value="Residential"
+                            checked={this.props.commercialOrResidential === 'Residential'}
+                            onChange={e => this.props.setCommercialOrResidential(e.target.value)}
+                        />
+
+                    </Form>
+                </Row>
                 <Form.Group>
                     <Form.Label>Reason For Request</Form.Label>
                     <FormControl
